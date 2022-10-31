baae8b498f28bc750e7fa7d94df3d44ebe703b69
julian-velez <60607869+julian-velez@users.noreply.github.com>
2022-10-31 12:49:45 -0500

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,45 @@
+//let nombre = "julian";
+
+//console.log(nombre);
+
+//let  edad = 15;
+//edad =  10
+//edad = 'julian';
+
+//console.log(edad)
+
+//let tieneCoche =  true;
+
+//console.log(tieneCoche)
+
+//let a = 5;
+//let b =  10;
+//let c = a + b;
+
+//console.log(a,b,c);
+
+//c = 50;
+//console.log(c);
+
+//c +=   20;
+//console.log(c);
+
+//c =  c + 1;
+//console.log(c)
+
+//let d = a *b;
+//console.log(a,b,d);
+
+let a = 7;
+let b = 4;
+
+/*
+32 = 10
+2 = 10
+*/
+
+console.log(a & b);
+
+
+
+
